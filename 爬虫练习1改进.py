06dff18faa56da2b35c7d499928ceffaddf5df98
Altiablack <1085525267@qq.com>
2025-09-16 20:27:49 +0800

    改正

--- 爬虫练习1改进.py
+++ 爬虫练习1改进.py
@@ -1,10 +1,8 @@
 import requests
-headers={"User-Agent":"Mozilla/5.0(Windows NT 10.0; Win64; x64)"}
-response=requests.get("https://www.bilibili.com/"，headers=header)
+head={"User-Agent":"Mozilla/5.0(Windows NT 10.0: Win64:x64)"}
+response=requests.get("https://www.bilibili.com/",headers=head)
 
 if response.ok:
     print(response.text)
 else:
     print("请求失败")
-       
-
